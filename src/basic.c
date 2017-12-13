59de05f3793047febb7887f8064426d497ea7272
Sang-Hoon Kim <sanghoon@vt.edu>
2017-12-13 16:55:47 -0500

    Make basic do more basic operation

--- src/basic.c
+++ src/basic.c
@@ -2,44 +2,29 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-
+#include <sys/time.h>
 #include <unistd.h>
-#include <sys/syscall.h>
-#include <sys/types.h>
-#include <fcntl.h>
-#include <string.h>
 
 #include "popcorn.h"
 
-void migrated_callback(void *params)
-{
-	char *buffer = params;
-	int fd;
-	fd = open("/tmp/basic-output", O_CREAT | O_WRONLY | O_APPEND, 0666);
-	write(fd, buffer, strlen(buffer));
-	close(fd);
-
-	return;
-}
-
 int main(int argc, char *argv[])
 {
-	time_t t;
-	struct tm *now;
-	char param[80];
+	unsigned long delay_ms;
 
-	t = time(NULL);
-	now = localtime(&t);
-	strftime(param, sizeof(param), "%Y%m%d_%H%M%S\n", now);
+	if (argc == 2) {
+		delay_ms = atol(argv[1]);
+	} else {
+		struct timeval time;
 
-	printf("Migrate me out!\n");
+		gettimeofday(&time, NULL);
+		srandom(time.tv_sec * time.tv_usec);
+		delay_ms = random() % 200 + 1; /* Up to 200 ms */
+	}
 
-	migrate(1, migrated_callback, param);
-	sleep(1);
-	migrate(0, migrated_callback, param);
+	printf("Travel for %ld ms\n", delay_ms);
+	migrate(1, NULL, NULL);
+	usleep(delay_ms * 1000);
+	migrate(0, NULL, NULL);
 
-	printf("I am back!\n");
-
-	return 0xdead;
+	return 0;
 }
