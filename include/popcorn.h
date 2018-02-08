8c9500f59186cc2d8a7fea4a124a1d5fa996144c
Sang-Hoon Kim <sanghoon@vt.edu>
2018-02-08 13:49:37 -0500

    Added macros for evaluation and testing

--- include/popcorn.h
+++ include/popcorn.h
@@ -5,9 +5,25 @@
 
 #define MAX_POPCORN_NODES	32
 
+/**
+ * Options to help performance evaluation
+ */
+#define _POPCORN_ALIGN_VARIABLES
+#define _POPCORN_ALIGN_HEAP
+#undef _POPCORN_PROFILE_REGION
+#undef _POPCORN_RELEASE_OWNERSHIP
+#ifndef PAGE_SHIFT
+#define PAGE_SHIFT	12
+#endif
+#ifndef PAGE_SIZE
+#define PAGE_SIZE	(1 << PAGE_SHIFT)
+#endif
+#ifndef PAGE_MASK
+#define PAGE_MASK	(~(PAGE_SIZE-1))
+#endif
 
 /**
- * Migrate this thread to the node nid. if nid == -1, use the proposed
+ * Migrate this thread to node @nid. if @nid == -1, use the proposed
  * migration location.
  *
  * return 0 on success, return non-zero otherwise
@@ -17,13 +33,27 @@
  */
 void migrate(int nid, void (*callback)(void *), void *callback_param);
 
+static inline void popcorn_migrate(int nid) {
+	migrate(nid, NULL, NULL);
+}
 
+/**
+ * Migrate thread specifiec by @popcorn_tid according to a thread schedule.
+ *
+ * @region:	a region identifier used to look up a mapping
+ * @popcorn_tid: a Popcorn-specific thread ID
+ */
 void migrate_schedule(size_t region,
                       int popcorn_tid,
                       void (*callback)(void *),
                       void *callback_data);
 
-/*
+static inline void popcorn_migrate_schedule (size_t region, int popcorn_tid) {
+	migrate_schedule(region, popcorn_tid, NULL, NULL);
+}
+
+
+/**
  * Get the current status of the current thread regarding migration
  *
  * return 0 on fetching the status successfully.
@@ -39,14 +69,13 @@ struct popcorn_thread_status {
 
 int popcorn_get_status(struct popcorn_thread_status *status);
 
-
-static inline int popcorn_current_nid(void)
-{
+static inline int popcorn_current_nid(void) {
 	struct popcorn_thread_status status;
 	if (popcorn_get_status(&status)) return -1;
 
 	return status.current_nid;
 }
+
 
 /**
  * Propose to migrate @tid to @nid
@@ -60,16 +89,50 @@ static inline int popcorn_current_nid(void)
 int popcorn_propose_migration(int tid, int nid);
 
 
+
 /**
  * Allocate memory aligned to the page boundary
  */
+#if defined(_POPCORN_ALIGN_VARIABLES) || defined(_POPCORN_ALIGN_HEAP)
+#define ALIGN_TO_PAGE __attribute__ ((aligned (PAGE_SIZE)))
+#else
+#define ALIGN_TO_PAGE
+#endif
+
+
+#ifdef _POPCORN_ALIGN_HEAP
 #include <malloc.h>
-static inline void *popcorn_malloc(size_t size)
-{
+static inline void *popcorn_malloc(size_t size) {
 	void *p = NULL;
-	posix_memalign(&p, 4096, size);
+	posix_memalign(&p, PAGE_SIZE, (size + PAGE_SIZE - 1) & PAGE_MASK);
 	return p;
 }
+#else
+static inline void *popcorn_malloc(size_t size) {
+	return malloc(size);
+}
+#endif /* _POPCORN_ALIGN_HEAP */
+
+
+/**
+ * Frequently used configurations
+ */
+#ifdef _POPCORN_ALIGN_VARIABLES
+struct __popcorn_config_t {
+	int configs[1024];
+} ALIGN_TO_PAGE;
+#else
+struct __popcorn_config_t {
+	int configs[2];
+};
+#endif /* _POPCORN_ALIGN_VARIABLES */
+
+#define POPCORN_DEFINE_CONFIGS()	\
+	struct __popcorn_config_t __popcorn_configs = { \
+		.configs = {0}, \
+	}
+#define POPCORN_CONFIG_CORES_PER_NODE	__popcorn_config.configs[0]
+#define POPCORN_CONFIG_NODES			__popcorn_config.configs[1]
 
 
 /**
@@ -79,16 +142,19 @@ static inline void *popcorn_malloc(size_t size)
 int popcorn_gettid();
 
 
+/**
+ * Get the popcorn node information
+ */
 enum popcorn_node_status {
 	POPCORN_NODE_OFFLINE = 0x00,
 	POPCORN_NODE_ONLINE = 0x01,
 };
 
 enum popcorn_arch_types {
+	POPCORN_NODE_UNKNOWN = -1,
 	POPCORN_NODE_AARCH64 = 0,
 	POPCORN_NODE_X86 = 1,
 	POPCORN_NODE_PPC64 = 2,
-	POPCORN_NODE_UNKNOWN,
 };
 
 struct popcorn_node_info {
@@ -97,93 +163,76 @@ struct popcorn_node_info {
 	int distance;
 };
 
-/**
- * Get the popcorn node information
- */
 int popcorn_get_node_info(struct popcorn_node_info *info);
+
+
+/**
+ *
+ */
+#define POPCORN_MIGRATE_START(tid) \
+	popcorn_migrate((tid) / POPCORN_CONFIG_CORES_PER_NODE)
+
+#define POPCORN_MIGRATE_END() \
+	popcorn_migrate(0)
+
+
+#ifdef _POPCORN_PROFILE_REGION
+#define PRCTL_TRACE_AUX		48
+#include <sys/prctl.h>
+static inline void popcorn_region_id(int id) {
+	prctl(PRCTL_TRACE_AUX, id);
+}
+#else
+static inline void popcorn_region_id(int id) {}
+#endif /* _POPCORN_PROFILE_REGION */
+
+
+#ifdef _POPCORN_RELEASE_OWNERSHIP
+#include <sys/mman.h>
+#define MADVISE_RELEASE 18
+static inline int popcorn_release_ownership(void *start, size_t len) {
+	return madvise(start, len, MADVISE_RELEASE);
+}
+#else
+static inline int popcorn_release_ownership(void *start, size_t len) { return -EINVAL };
+#endif /* _POPCORN_RELEASE_OWNERSHIP */
+
+
+#define POPCORN_MIGRATE_REGION_START(tid,R) \
+	POPCORN_MIGRATE_START(tid); \
+	popcorn_region_id(R);
+
+#define POPCORN_MIGRATE_REGION_END() \
+	popcorn_region_id(0); \
+	POPCORN_MIGRATE_END();
 
 
 #ifdef _OPENMP
 #include <omp.h>
 
-/**
- * Helper functions to transform omp "parallel for" to "parallel"
- * Example:
- *
- *   #ifndef _POPCORN
- *   #pragma omp parallel for default(shared) private(i)
- *   for (i = 0; i <= 10; i++) {
- *   #else
- *   #pragma omp prallel default(shared)
- *   {
- *   int i;
- *   POPCORN_OMP_SPLIT_START(i, 0, 10, THREADS_PER_CORE);
- *   #endif
- *         ...
- *   #ifdef _POPCORN
- *   POPCORN_OMP_SPLIT_END();
- *   }
- *   #endif
- */
-static inline void popcorn_omp_split(int tid, int threads,
-		int start, int end, int *range_start, int *range_end)
-{
-	int N = end - start + 1;
-	float step;
-	if (N < threads) {
-		threads = N;
-		if (tid >= N) {
-			*range_start = 0;
-			*range_end = -1;
-			return;
-		}
-	}
-	step = (float)N / threads;
-	*range_start = start + tid * step;
-	if (tid != threads - 1) {
-		*range_end = start + (tid + 1) * step - 1;
-	} else {
-		*range_end = end;
-	}
-	/*
-	printf("%d : %d - %d, %d\n", tid, *range_start, *range_end,
-			*range_end - *range_start + 1);
-	*/
-}
-
-
-#if 0
-#define POPCORN_OMP_SPLIT_START(k, start, end, N) \
+#ifdef SCHEDULE_THREADS
+#define POPCORN_OMP_MIGRATE_START(R) \
 {	\
-	int _s, _e; \
-	const int _tid = omp_get_popcorn_tid(); \
-	popcorn_omp_split(_tid, omp_get_max_threads(), (start), (end), &_s, &_e); \
-	migrate_schedule(0, _tid, NULL, NULL); \
-	popcorn_omp_id(R); \
-	for ((k) = _s; (k) <= _e; (k)++)
+	popcorn_migrate_schedule(R, omp_get_popdorn_tid()); \
+	popcorn_region_id(R); \
 
 
-#define POPCORN_OMP_SPLIT_END() \
-	popcorn_omp_id(0);
-	migrate(0, NULL, NULL); \
+#define POPCORN_OMP_MIGRATE_END() \
+	popcorn_region_id(0); \
+	popcorn_migrate(0);
 }
 
-#else
-#define POPCORN_OMP_SPLIT_START(k, start, end, N) \
-{	\
-	int _s, _e; \
-	const int _tid = omp_get_thread_num(); \
-	const int _cores = N; \
-	popcorn_omp_split(_tid, omp_get_max_threads(), (start), (end), &_s, &_e); \
-	if (_tid / _cores) migrate(_tid / _cores, NULL, NULL); \
-	for ((k) = _s; (k) <= _e; (k)++)
+#else /* SCHEDULE_THREADS */
+#define POPCORN_OMP_MIGRATE_START(R) \
+{ \
+	popcorn_migrate(omp_get_thread_num() / POPCORN_CONFIG_CORES_PER_NODE); \
+	popcorn_region_id(R);
 
-
-#define POPCORN_OMP_SPLIT_END() \
-	if (_tid / _cores) migrate(0, NULL, NULL); \
+#define POPCORN_OMP_MIGRATE_END() \
+	popcorn_region_id(0); \
+	popcorn_migrate(0); \
 }
-#endif
-
+#endif /* SCHEDULE_THREADS */
 #endif /* _OPENMP */
 
 #endif
